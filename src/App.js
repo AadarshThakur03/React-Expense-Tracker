70056550689a677703a6007a2ce40cbeb26fd50e
adarshT-03 <thakuradarsh8101@gmail.com>
2025-03-11 03:22:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Transaction from "./Pages/Transaction";
+import Report from "./Pages/Report";
+import Navbar from "./Components/Navbar";
+import NotFound from "./Pages/NotFound";
+import AddTransaction from "./Pages/AddTransaction";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/transaction" element={<Transaction/>}/>
+          <Route path="/reports" element={<Report/>}/>
+          <Route path="/add-transaction" element={<AddTransaction/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+       
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
